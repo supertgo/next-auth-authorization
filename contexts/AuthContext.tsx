a5f50999a655e54507b627ca5aaff63b4400892b
supertgo <thiagoroberto2003@gmail.com>
2022-02-19 08:01:36 -0300

    feat: :sparkles: retrieve auth state

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,5 +1,6 @@
 import Router from "next/router";
-import { createContext, useState } from "react";
+import { setCookie, parseCookies } from 'nookies'
+import { createContext, useEffect, useState } from "react";
 import { api } from "../services/api";
 
 type User = {
@@ -29,6 +30,18 @@ export function AuthProvider({children}: AuthPRoviderProps) {
   const [user, setUser] = useState<User>()
   const isAuthenticated = !!user
 
+  useEffect(() => {
+    const {'nextauth.token': token } = parseCookies()
+
+    if(token) {
+      api.get('me').then(response => {
+       const { email, permissions, roles } = response.data
+
+       setUser({email, permissions, roles})
+      })
+    }
+  }, [])
+
   async function signIn({email, password}: SignInCredentials) {
     try {
       const response = await api.post('sessions', {
@@ -36,13 +49,24 @@ export function AuthProvider({children}: AuthPRoviderProps) {
         password
       })
 
-      const { toke, refreshToken, permissions, roles } = response.data
+      const { token, refreshToken, permissions, roles } = response.data
+
+      setCookie(undefined, 'nextauth.token', token, {
+        maxAge: 60 * 60 * 24 * 30, //30 days
+        path: '/'
+      })
+      setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, //30 days
+        path: '/'
+      })
    
       setUser({
         email,
         permissions,
         roles
       })
+
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`
 
       Router.push('/dashboard')
 
