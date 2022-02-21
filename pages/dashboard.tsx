9e97f36f32df0f4042920c7eee461931fe7cb9e0
supertgo <thiagoroberto2003@gmail.com>
2022-02-21 08:06:50 -0300

    feat(api): :sparkles: create permission hook

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -2,21 +2,32 @@ import { destroyCookie } from "nookies"
 import { useContext, useEffect } from "react"
 import { AuthContext } from "../contexts/AuthContext"
 import { AuthTokenError } from "../errors/AuthTokenError"
+import { useCan } from "../hooks/useCan"
 import { setupApiClient } from "../services/api"
 import { api } from "../services/apiClient"
 import { withSRRAuth } from "../utils/withSSRAuth"
 
 export default function Dashboard() {
   const { user }  = useContext(AuthContext)
-
-
+  const userCanSeeMetrics = useCan({
+    permissions: ['metrics.list']
+  })
 
   useEffect(() => {
     api.get('/me')
       .then(response => console.log(response))
       .catch(err => console.log(err))
   }, [])
-  return <h1>Dashboard {user?.email}</h1>
+
+
+  return (
+    <>
+      <h1>Dashboard {user?.email}</h1>
+      {userCanSeeMetrics && <div>Métricas</div>}
+    </>
+  )
+  
+  
 } 
 
 export const getServerSideProps = withSRRAuth(async (ctx) => {
